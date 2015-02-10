0eebbb4fc4e5a087464f4c1afbad5401da8f4248
Foxman <ron.mikhlin7@gmail.com>
2015-02-10 20:35:15 +0200

    multithreading and loop now working

--- FoxChat_Client.py
+++ FoxChat_Client.py
@@ -1,18 +1,36 @@
 from Tkinter import *
 import time
+import thread
 
 root = Tk()
-root.geometry ("700x500")
-root.title ("FoxChat Client")
 lb = Label(root, text = "1")
-lb.pack()
 
-while 1:
-    time.sleep (1000)
+def main():
+    
+    root.geometry ("700x500")
+    root.title ("FoxChat Client")
+    lb.pack()
+
     lb.text = "2"
-    time.sleep (1000)
-    lb.text = "1"
     
-    
+    try:
+      thread.start_new_thread(rotater,())
+    except:
+      print "Error: unable to start thread"
+      
+    root.mainloop()
 
-root.mainloop()
+
+def rotater():
+    print "thread started"
+    while 1:
+        time.sleep (1)
+        print "printing..."
+        lb['text'] = "2"
+        time.sleep (1)
+        print "printing..."
+        lb['text'] = "1"
+
+
+if __name__ == "__main__":
+  main()
