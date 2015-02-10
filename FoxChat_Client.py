f508ea09ca1adbab8d65fd773eb7826e7a3b6844
Foxman <ron.mikhlin7@gmail.com>
2015-02-10 21:17:01 +0200

    Added connection form
    username and ip button and label

--- FoxChat_Client.py
+++ FoxChat_Client.py
@@ -1,17 +1,40 @@
 from Tkinter import *
+import tkFont
 import time
 import thread
 
 root = Tk()
-lb = Label(root, text = "1")
+username = ""
+servIP = ""
+
+ipEntry = Entry(root, width = 30,)
+userEntry = Entry(root, width = 30)
 
 def main():
     
     root.geometry ("700x500")
     root.title ("FoxChat Client")
-    lb.pack()
+    
+    ipLabel = Label(root,
+               text = "Enter chat server IP: ",
+               relief = "ridge",
+               font = tkFont.Font(size = 15))
+    
+    
+    ipLabel.grid(row = 0)   
+    ipEntry.grid(row = 0, column = 1)
 
-    lb.text = "2"
+    userLabel = Label(root,
+               text = "Enter your chat name: ",
+               relief = "ridge",
+               font = tkFont.Font(size = 15))
+    
+    userLabel.grid(row = 1)   
+    userEntry.grid(row = 1, column = 1)
+
+    connectButton = Button(root, text = "Connect", command = connect)
+    connectButton.grid(row = 2, column = 1)
+    
     
     try:
       thread.start_new_thread(rotater,())
@@ -20,17 +43,15 @@ def main():
       
     root.mainloop()
 
+def connect():
+    servIP = ipEntry.get()
+    username = userEntry.get()
+    print servIP
+    print username
 
 def rotater():
-    print "thread started"
     while 1:
-        time.sleep (1)
-        print "printing..."
-        lb['text'] = "2"
-        time.sleep (1)
-        print "printing..."
-        lb['text'] = "1"
-
+        time.sleep(1000)
 
 if __name__ == "__main__":
   main()
