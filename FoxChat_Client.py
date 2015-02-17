1601cff479f15dc89ad279158fd948ea3c0619ef
Foxman <ron.mikhlin7@gmail.com>
2015-02-17 19:07:33 +0200

    list crap

--- FoxChat_Client.py
+++ FoxChat_Client.py
@@ -7,16 +7,17 @@ import pickle
 root = Tk()
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-BUFFER_SIZE = 5120
-SERV_PORT = 1740
+BUFFER_SIZE = 1024
+SERV_PORT = 1997
 username = ""
 servIP = ""
 
 ipEntry = Entry(root, width = 30,)
 userEntry = Entry(root, width = 30)
+msgEntry = Entry(root, width = 40)
 
 chatList = []
-chatListBox = ListBox(root)
+chatListBox = Listbox(root)
 
 def main():
     
@@ -42,27 +43,54 @@ def main():
 
     connectButton = Button(root, text = "Connect", command = connect)
     connectButton.grid(row = 2, column = 1)
+    discoconnectButton = Button(root, text = "Disconnect", command = disconnect)
+    discoconnectButton.grid(row = 3, column = 1)
 
-	chatListBox.grid(row = 4)
+    chatListBox.grid(row = 4)
+    chatListBox['height'] = 20
+    chatListBox['width'] = 50
+    chatListBox.insert(0, "Python")
+    chatListBox.delete(0)
+    chatListBox.insert(0, "yay")
 
-      
+
+    msgEntry.grid(row = 5, column = 0)
+
+    chatSendButton = Button(root, text = "Send", command = send)
+    chatSendButton.grid(row = 6, column = 0)
+    
     root.mainloop()
 
 def connect():
     servIP = ipEntry.get()
     username = userEntry.get()
     if servIP != "" and username != "":
-        sock.connect(servIP, SERV_PORT)      
+        sock.connect((servIP, SERV_PORT))      
         try:
-            thread.start_new_thread(chat,())
+            thread.start_new_thread(recieve,())
         except:
             print "Error: unable to start thread"
 
-def chat():
+def recieve():
+    print "connected"
     while 1:
-        chatList = pickle.loads(conn.recv(BUFFER_SIZE))
-		for msg in chatList:
-			chatListBox.insert(END, msg)
+        chatList = pickle.loads(sock.recv(BUFFER_SIZE))
+        i = 0
+        for msg in chatList:
+            chatListBox.delete(i)
+            chatListBox.insert(i, msg)
+            i += 1
+
+def send():
+    msg = msgEntry.get()
+    if(msg != ""):
+        msg = username + msg
+        print msg
+        sock.send(msg)
+
+def disconnect():
+    sock.close()
+    
 		   
 if __name__ == "__main__":
   main()
